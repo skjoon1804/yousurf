7f8e752ae6a2cab9d6dcce650b05d7036ca36f6d
skjoon1804 <skjoon1804@naver.com>
2021-02-13 00:19:44 -0800

    Add random video search - feeling lucky

--- src/app/components/SearchBar/SearchBar.js
+++ src/app/components/SearchBar/SearchBar.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { setSearchQuery, setResults, setNextPageToken } from '../../actions'
 import { connect } from 'react-redux';
 import './SearchBar.css';
-
+import randomWords from 'random-words';
 import { initialRequest } from '../../../apis/youtube';
 
 const SearchBar = ({
     query, results,
-    setSearchQuery, setResults, setNextPageToken
+    setRandomQuery, setSearchQuery, setResults, setNextPageToken
 }) => {
 
     async function fetchResult(e) {
@@ -28,21 +28,22 @@ const SearchBar = ({
         {results.length === 0 
         ?
             <div className="text-center align-items-center justify-content-center d-flex flex-column h-100">
-                <h1>YouSurf</h1>
+                <h1 id="title" className="display-2 m-3">YouSurf</h1>
                 <form onSubmit={fetchResult} className="m-4 w-100">
-                    <input type="text" name="query" placeholder="Enter Keyword" 
-                    className="d-block m-auto input-group-lg w-50" onChange={setSearchQuery} />
-                    <button type="submit" className="btn btn-outline-dark m-2 px-5">Search</button>
-                    <button className="btn btn-outline-dark m-2 px-4">I'm Feeling Lucky</button>
+                    <div className="mb-4">
+                        <input type="text" name="query" placeholder="Enter Keyword" 
+                            className="d-block m-auto input-group-lg w-50 form-control-lg" onChange={setSearchQuery} />
+                    </div>
+                    <button type="submit" id="search-button" className="btn btn-lg btn-outline-dark m-2 px-5">Search</button>
+                    <button type="submit" id="search-button" className="btn btn-lg btn-outline-dark m-2 px-4" onClick={setRandomQuery}>I'm Feeling Lucky</button>
                 </form>
             </div>
         :
-            <div className="text-center container">
-                <h2>YouSurf</h2>
+            <div id="continue-search" className="text-center container p-5">
+                <h2 id="title">YouSurf</h2>
                 <form onSubmit={fetchResult} className="m-4">
-                    <input type="text" name="query" placeholder="Enter Keyword" 
-                    className="d-block m-auto input-group-lg w-50" onChange={setSearchQuery} />
-                    <button type="submit" className="btn btn-outline-dark m-2 px-5">Search</button>
+                    <input type="text" name="query" placeholder="Enter Keyword" className="d-block m-auto input-group-lg w-50" onChange={setSearchQuery} />
+                    <button type="submit" id="search-button" className="btn btn-outline-dark m-2 px-5">Search</button>
                 </form>
             </div>
         }
@@ -59,7 +60,11 @@ const mapStateToProps = state => {
 }
 
 const mapDispatchToProps = (dispatch) => {
+    const randomQuery = randomWords();
     return {
+        setRandomQuery(e) {
+            dispatch(setSearchQuery(randomQuery));
+        },
         setSearchQuery(e) {
             dispatch(setSearchQuery(e.target.value));
         },
