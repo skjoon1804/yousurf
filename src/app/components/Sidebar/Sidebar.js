3a9324264cdf4c226bbfcbc455c961138d629efe
skjoon1804 <skjoon1804@naver.com>
2021-02-14 14:16:00 -0800

    Update sidebar design

--- src/app/components/Sidebar/Sidebar.js
+++ src/app/components/Sidebar/Sidebar.js
@@ -5,7 +5,6 @@ import './Sidebar.css'
 import { processAuthenticateUser } from '../../actions'
 import { history } from '../../reducers/history';
 
-
 const openNav = () => { document.getElementById("sidenav").style.width = "250px"; }
 const closeNav = () => { document.getElementById("sidenav").style.width = "0"; }
 
@@ -16,11 +15,11 @@ export const Sidebar = ({name, logout}) => {
             <div className="sidenav" id="sidenav">
                 <h5 className="m-3">Welcome, {name}</h5>
                 <a href="#" className="closebtn" onClick={closeNav}>&times;</a>
-                <Link to="/home">Home</Link>
-                <Link to="/profile">Profile</Link>
-                <Link to="/favorite">Favorite</Link>
+                <Link className="menu-item" to="/home">Home</Link>
+                <Link className="menu-item" to="/profile">Profile</Link>
+                <Link className="menu-item" to="/favorite">Favorite</Link>
                 <div className="dropdown-divider"/>
-                <a href="/" onClick={logout}>Logout</a>
+                <a href="" onClick={logout} className="menu-item">Logout</a>
             </div>
             <span className="m-4" style={{fontSize: '30px', cursor: 'pointer', float: 'right'}} onClick={openNav}>&#9776;</span>
         </>
@@ -29,16 +28,13 @@ export const Sidebar = ({name, logout}) => {
 
 const mapStateToProps = (state) => {
     const { name } = state.users.find(user => user.id === state.session.id);
-    return {
-        name
-    };
+    return { name };
 }
 
 const mapDispatchToProps = (dispatch) => {
     return {
         logout() {
-            dispatch(processAuthenticateUser(""));
-            history.push("/");
+            dispatch(processAuthenticateUser(null));
         }
     }
 }
